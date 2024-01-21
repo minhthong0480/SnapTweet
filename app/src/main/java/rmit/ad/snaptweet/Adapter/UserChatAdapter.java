c82adbde0fae4b0a5e020460277400226b0bba81
Minh Thong <s3852882@rmit.edu.vn>
2024-01-21 23:27:34 +0700

    working with display chat

--- app/src/main/java/rmit/ad/snaptweet/Adapter/UserChatAdapter.java
+++ app/src/main/java/rmit/ad/snaptweet/Adapter/UserChatAdapter.java
@@ -82,14 +82,12 @@ public class UserChatAdapter extends RecyclerView.Adapter<UserChatAdapter.ViewHo
         public TextView username;
         public TextView fullname;
         public CircleImageView image_profile;
-        public Button btn_follow;
         public ViewHolder(@NonNull View itemView){
             super(itemView);
 
             username = itemView.findViewById(R.id.username);
             fullname = itemView.findViewById(R.id.fullname);
             image_profile = itemView.findViewById(R.id.image_profile);
-            btn_follow = itemView.findViewById(R.id.btn_follow);
         }
     }
 }
--- app/src/main/java/rmit/ad/snaptweet/Fragment/ChatFragment.java
+++ app/src/main/java/rmit/ad/snaptweet/Fragment/ChatFragment.java
@@ -2,6 +2,7 @@ package rmit.ad.snaptweet.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -10,19 +11,115 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rmit.ad.snaptweet.Adapter.UserAdapter;
+import rmit.ad.snaptweet.Adapter.UserChatAdapter;
+import rmit.ad.snaptweet.Model.Chat;
+import rmit.ad.snaptweet.Model.User;
 import rmit.ad.snaptweet.R;
 
 
 public class ChatFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private UserChatAdapter userAdapter;
+    private List<User> mUsers;
+
+    FirebaseUser fuser;
+    DatabaseReference reference;
+
+    private List<String> userList;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        userList = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userList.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Chat chat = snapshot.getValue(Chat.class);
+
+                    if (chat.getSender().equals(fuser.getUid())){
+                        userList.add(chat.getReceiver());
+                    }
+                    if (chat.getReceiver().equals(fuser.getUid())){
+                        userList.add(chat.getSender());
+                    }
+                }
+
+                readChats();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         return view;
+    }
+
+    private void readChats(){
+        mUsers = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUsers.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+
+                    for (String id : userList){
+                        if(user.getId().equals(id)){
+                            if(mUsers.size() != 0){
+                                for (User user1 : mUsers){
+                                    if (!user.getId().equals(user1.getId())){
+                                        mUsers.add(user);
+                                    }
+                                }
+                            } else{
+                                mUsers.add(user);
+                            }
+                        }
+                    }
+                }
+
+                userAdapter = new UserChatAdapter(getContext(), mUsers);
+                recyclerView.setAdapter(userAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
